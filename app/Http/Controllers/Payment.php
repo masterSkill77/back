583e0337b9ecf2a8ec84d1381bcb2e212d76c2d0
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2022-05-15 00:19:15 +0300

    fix bug

--- app/Http/Controllers/Payment.php
+++ app/Http/Controllers/Payment.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Binance\API;
 use Illuminate\Http\Request;
 
 class Payment extends Controller
 {
-    //
+    public function payer(Request $request)
+    {
+        $api = new API("VCUzIWeKEZkJcxIgTvKRNUnPy0yJ4n9i5akLRfoRD8pFX4IWKxz09z6BZihL3e83", "eyJpdiI6IklsOUVwSTNpeXFqNTlRc0RGQkl0U2c9PSIsInZhbHVlIjoiT0ZVSEk3bjVQazgzSGlKK3JKdE0zbmttd2JlbHRhME5lNjBoVm8zZnJIZHZicyttVDkxVjZlQU1TQXE4RldWNjZkbXJEcXlnWndZZytZL2l4ZkxzT3dIZTV5ZTJHVkpXTW4zK0dkYzkwUUU9IiwibWFjIjoiOWNkNTRmM2VkMGEzZWUyMTE0YWJkNzNkMjk5MDYwZWM3ZDI3ZjIwN2IwZDgxNGNmNjRmYWEyYWRjZDRkYjRlMiIsInRhZyI6IiJ9", true);
+        // $api = new API($request->input('public'), $request->input('secret'));
+
+
+    }
 }
